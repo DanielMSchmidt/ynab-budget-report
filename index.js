b61afcf21be9cca17695f528c88689754af221ce
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-22 20:12:15 +0200

    lets get some data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const inquirer = require("inquirer");
+const moment = require("moment");
 
 const API_ENDPOINT = "https://api.youneedabudget.com/v1";
 const API_TOKEN = process.env.TOKEN;
@@ -25,8 +26,23 @@ async function getCategories(budgetId) {
   return response.data.category_groups;
 }
 
+async function getBudgetForMonth(budgetId, month) {
+  const url =
+    API_ENDPOINT +
+    "/budgets/" +
+    budgetId +
+    "/months/" +
+    month.format("YYYY-MM-DD");
+  console.log(url);
+  const response = await fetch(url, options).then(resp => resp.json());
+  console.log(response)
+  return response.data.month.categories;
+}
+
 (async function loop() {
   const budget = await getActiveBudget();
+  const firstMonth = moment(budget.first_month).day(0);
+  const lastMonth = moment(budget.last_month).day(0);
   const categories = await getCategories(budget.id);
   const flatCategories = categories.reduce(
     (list, item) => list.concat(item.categories),
@@ -42,6 +58,23 @@ async function getCategories(budgetId) {
     }
   ]);
 
-  const categoryChoice = flatCategories.find(cat => cat.name === category);
-  console.log("category: ", categoryChoice);
+
+  let currentMonth = firstMonth.clone();
+  const categoryForMonths = [];
+  while (currentMonth.isBefore(lastMonth)) {
+    getBudgetForMonth(budget.id, currentMonth);
+    const categoriesForMonth = await getBudgetForMonth(budget.id, currentMonth);
+
+    const selectedCategoryForMonth = categoriesForMonth.find(cat => cat.name === category);
+    categoryForMonths.push(selectedCategoryForMonth);
+
+    currentMonth.add(1, "month");
+  }
+
+  console.log(categoryForMonths);
+
+
+  // For each month since start of budget
+  // GET /budgets/{budget_id}/months/{month} => categories
+  // Draw this as pdf chart
 })();
